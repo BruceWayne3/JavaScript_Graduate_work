373f7883225de03937ed9aa46c2950f95f14d0ed
Ilya Kyazimov <40570012+BruceWayne3@users.noreply.github.com>
2018-11-09 15:43:37 +0300

    Update game.js

--- game.js
+++ game.js
@@ -185,7 +185,8 @@ class LevelParser {
 
     createGrid(arrayStr) {
         return arrayStr.map(noMoveArray =>
-                            [...noMoveArray].map(cell => this.obstacleFromSymbol(cell)));
+                            [...noMoveArray].map(cell =>
+                                                 this.obstacleFromSymbol(cell)));
     }
 
     createActors(arrayStr) {
